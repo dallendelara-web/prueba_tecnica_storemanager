5193a28fa06a933c72eb0478d6b8239fefaa570d
Denisse Allende <dallendelara@gmail.com>
2026-03-27 13:54:48 -0600

    Feature/ Integración de filtros en los usuarios, desde el lado del cliente

--- src/app/main/UsersCarts.tsx
+++ src/app/main/UsersCarts.tsx
@@ -25,6 +25,7 @@ import {
 } from "lucide-react";
 
 import { IsEmpty } from '@/helpers/ValidateValue';
+import { filtered } from "@/helpers/FilterArray"
 
 import WatingData from "@/components/LottieAnimations/WatingData";
 import NotDataFound from "@/components/LottieAnimations/NotDataFound";
@@ -36,14 +37,17 @@ const UsersCarts = () => {
 
     const [open, setOpen] = React.useState(false);
     const [search, setSearch] = useState("");
-    const [filterData, setFilterData] = useState<string>("");
+    const [filterData, setFilterData] = useState<string>("all");
     const debouncedSearchTerm = useDebounce(search, 300);
 
     const [usersList, setUsersList] = useState([]);
+    const [usersFullList, setUsersFullList] = useState([]);
     const [totalDocs, setTotalDocs] = useState(0);
     const [limit, setLimit] = useState(12);
     const [page, setPage] = useState(1);
+    const [skipt, setSkipt] = useState(0);
     const [totalPages, setTotalPages] = useState(1);
+    const [searchPlaceholder, setSearchPlaceholder] = useState("Seleccione un filtro...");
 
     useEffect(() => {
         if(user?.id && !isLoadingGetUsers){
@@ -52,12 +56,30 @@ const UsersCarts = () => {
     }, []);
 
     useEffect(() => {
-            if(getUsersSuccess?.data?.users?.length > 0){
+        if(getUsersSuccess?.data?.users?.length > 0){
+            const res = getUsersSuccess?.data?.users.map(function (ev: any) {
+                return ({
+                    ...ev,
+                    fullName: ev?.firstName+ " " + ev?.lastName
+                })
+            });
             setTotalDocs(getUsersSuccess?.data?.total)
-            setUsersList(getUsersSuccess?.data?.users);
+            setUsersFullList(res);
+            if(filterData == "all"){
+                setUsersList(res);
+            }else{
+                //Función para filtrar
+                if (!IsEmpty(search.trim())) {
+                    const filteredData = filtered(res, filterData, search.trim());
+                    setUsersList(filteredData);
+                }else{
+                    setUsersList(res);
+                }
+            }
         }else{
             setTotalDocs(0)
             setUsersList([]);
+            setUsersFullList([]);
         }
     }, [getUsersSuccess]);
 
@@ -69,27 +91,43 @@ const UsersCarts = () => {
     }, [totalDocs]);
 
     useEffect(() => {
-            
+        setSearchPlaceholder("Seleccione un filtro...");
+        if(filterData == "all" && usersFullList?.length > 0 && search?.trim() != ""){
+            setSearch("");
+            getUsers(limit?.toString(), skipt?.toString());
+        }
+        else if(filterData == "fullName"){
+            setSearchPlaceholder("Buscar por Nombre de usuario..");
+            executeDebunced();
+        }
+        else if(filterData == "email"){
+            setSearchPlaceholder("Buscar por Email..");
+            executeDebunced();
+        }
     }, [filterData]);
 
     useEffect(() => {
-            if (debouncedSearchTerm) {
-            if (IsEmpty(search.trim())) {
-                //getProductsList(limit?.toString(), "0");
-            } else {
-                //getProductsByCategory(search.trim(), limit?.toString(), "0")
-            }
+        if (debouncedSearchTerm && filterData !=="all") {
+            executeDebunced();
         }
     }, [debouncedSearchTerm]);
+
+    const executeDebunced = ()=> {
+        if (IsEmpty(search.trim())) {
+            //
+        } else {
+            //Función para filtrar
+            const filteredData = filtered(usersFullList, filterData, search.trim());
+            setUsersList(filteredData);
+        }
+    }
 
     const handleChange = (event, value) => {
         const skipt = value == 1 ? 0 : limit * (value-1);
         setPage(value);
-        if (IsEmpty(search.trim())){
-            //getProductsList(limit?.toString(), skipt.toString());
-        }else{
-            //getProductsByCategory(search.trim(), limit?.toString(), skipt.toString())
-        }
+        setSkipt(skipt);
+        getUsers(limit?.toString(), skipt.toString());
+        
     };
 
     return(
@@ -102,7 +140,7 @@ const UsersCarts = () => {
                 <FilterComponent
                     search={search}
                     setSearch={setSearch}
-                    searchPlaceholder={""}
+                    searchPlaceholder={searchPlaceholder ?? ""}
                     actions={
                         <div className="flex flex-row gap-x-2">
                             <Select value={filterData} onValueChange={setFilterData}>
@@ -111,8 +149,9 @@ const UsersCarts = () => {
                                 <SelectValue placeholder="Buscar por" />
                             </SelectTrigger>
                             <SelectContent>
-                                <SelectItem value="user_name">Nombre usuario</SelectItem>
-                                <SelectItem value="e_mail">Email</SelectItem>
+                                <SelectItem value="all">Mostrar todos</SelectItem>
+                                <SelectItem value="fullName">Nombre usuario</SelectItem>
+                                <SelectItem value="email">Email</SelectItem>
                             </SelectContent>
                             </Select>
                         </div>
@@ -130,9 +169,10 @@ const UsersCarts = () => {
                             <div className="grid gap-x-2 gap-y-4 mt-5">
                                 <div  className="grid gap-x-2 gap-y-4 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
                                     {
-                                        usersList?.map((user) => (
+                                        usersList?.map((user, idx) => (
                                             <UserCartCard
-                                                nombre={user?.firstName +" "+user?.lastName}
+                                                key={idx}
+                                                nombre={user?.fullName}
                                                 email={user?.email}
                                                 numeroCarritos={user?.id}
                                                 onAction={()=>{console.log("Show drawer")}}
